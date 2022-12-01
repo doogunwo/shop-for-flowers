e04d12f2c8b47a3035204b5c156fea49e330d8d7
doogunwo <dgw0601@naver.com>
2022-12-01 09:07:12 +0900

    gui add

---
+++ src/shop_for_flowers_UI/myshoppingList.java
@@ -0,0 +1,43 @@
+package shop_for_flowers_UI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class myshoppingList {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					myshoppingList window = new myshoppingList();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public myshoppingList() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
